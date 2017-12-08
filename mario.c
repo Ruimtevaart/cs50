c7bdd380518f4c014ab0fc24d3a57b7d3c521831
Xgrantos <Xgrantos@users.noreply.github.com>
2017-12-08 16:06:13 +0000

    20171208T160603Z

--- mario.c
+++ mario.c
@@ -4,26 +4,32 @@
 int main(void)
 {
     printf("Height: ");
-    int height=get_int();
-    while(height>23||height<0)
+    int height = get_int();
+    while (height > 23 || height < 0)
     {
         printf("Invalid height, please enter another one (between 1 and 23) ");
-        height=get_int();
+        height = get_int();
     }
-    if (height==0)
+    if (height == 0)
     {
         return 0;
     }
-    for(int i=0; i<height; i++)
+    for (int i = 0; i < height; i++)
     {
         int a, b, c;
-        for(a=0; a<height-i-1; a++)
-        printf(" ");
-        for(b=0; b<height-a; b++)
-        printf("#");
+        for (a = 0; a < height - i - 1; a++)
+        {
+            printf(" ");
+        }
+        for (b = 0; b < height - a; b++)
+        {
+            printf("#");
+        }
         printf("  ");
-        for(c=0; c<b; c++)
-        printf("#");
+        for (c = 0; c < b; c++)
+        {
+            printf("#");
+        }
         printf("\n");
     };
 }
