e6bc6371ac9f2cd1bf33080ad1ad2c7afd61b75f
Xgrantos <Xgrantos@users.noreply.github.com>
2018-04-17 19:34:29 +0000

    20180417T193321Z

---
+++ mario.py
@@ -0,0 +1,15 @@
+from cs50 import get_int
+
+
+def main():
+    height = get_int("Height: ")
+    while height > 23 or height < 0
+        height = get_int("Height: ")
+    for i in range(height):
+        a = height - i - 1
+        b = height - a
+        print(" " * a, "#" * b, "  ", "#" * b)
+
+
+if __name__ == "__main__":
+    main()
